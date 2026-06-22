e5e3627c77ca62827208a053d8afb06ad0cf7ba7
narendrapaduchuri <115544611+narendrapaduchuri@users.noreply.github.com>
2026-06-22 16:06:34 +0100

    DTR-6086-fix FAR updated URL's (#25)

--- test/views/NetTakingsLowerViewSpec.scala
+++ test/views/NetTakingsLowerViewSpec.scala
@@ -37,7 +37,7 @@ class NetTakingsLowerViewSpec extends SpecBase {
       doc.select("h1").text mustBe messages("netTakingsLower.heading")
       doc.select(".govuk-caption-l").text mustBe messages("netTakingsLower.caption", "1 Jan 2014", "31 Dec 2014")
       doc.select(".govuk-input__prefix").text mustBe "£"
-      doc.select("input.govuk-input").hasClass("govuk-input--width-15") mustBe true
+      doc.select("input.govuk-input").hasClass("govuk-input--width-20") mustBe true
       doc.select("button").text mustBe messages("site.continue")
     }
 
