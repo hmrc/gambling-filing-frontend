1a10f6c6532646a0f972c02f20717c6bead05415
JulianSaunders <30262076+JulianSaunders@users.noreply.github.com>
2026-07-14 15:21:15 +0100

    DTR_5580_RegNumber_validation

--- app/controllers/actions/AuthorisedAction.scala
+++ app/controllers/actions/AuthorisedAction.scala
@@ -64,7 +64,11 @@ class DefaultAuthorisedAction @Inject() (
               regNum,
               regime
             ) =>
-          block(AuthorisedRequest(request, affinityGroup, regNum, regime))
+          if (ValidationAction.validateRegimeRegNo(regime, regNum)) {
+            block(AuthorisedRequest(request, affinityGroup, regNum, regime))
+          } else {
+            Future.successful(Redirect(controllers.routes.AccessDeniedController.onPageLoad()))
+          }
         case Some(AffinityGroup.Organisation) ~ _ =>
           logger.warn(s"Organisation auth failed: enrolment missing or not activated for ${request.path}")
           Future.failed(InsufficientEnrolments("Organisation enrolment missing or not activated"))
@@ -126,5 +130,81 @@ object AuthorisedAction {
   object HasActiveOrganisationEnrolment {
     def unapply(enrolments: Enrolments): Option[(String, Regime)] =
       findActiveEnrolment(enrolments, organisationEnrolments)
+  }
+
+}
+
+object ValidationAction extends Logging {
+  private val REF_NO_LENGTH = 7
+  private val regEx = "X[A-Z]{1}[A-Z]{1}[0-9]{11}"
+
+  private val WEIGHT_9 = 9
+  private val WEIGHT_10 = 10
+  private val WEIGHT_11 = 11
+  private val WEIGHT_12 = 12
+  private val WEIGHT_13 = 13
+  private val WEIGHT_8 = 8
+  private val WEIGHT_7 = 7
+  private val WEIGHT_6 = 6
+  private val WEIGHT_5 = 5
+  private val WEIGHT_4 = 4
+  private val WEIGHT_3 = 3
+  private val WEIGHT_2 = 2
+
+  private val weights =
+    List(WEIGHT_9, WEIGHT_10, WEIGHT_11, WEIGHT_12, WEIGHT_13, WEIGHT_8, WEIGHT_7, WEIGHT_6, WEIGHT_5, WEIGHT_4, WEIGHT_3, WEIGHT_2)
+  private val checkChars = List("A", "B", "C", "D", "E", "F", "G", "H", "X", "J", "K", "L", "M", "N", "Y", "P", "Q", "R", "S", "T", "Z", "V", "W")
+
+  def validateRegimeRegNo(regime: Regime, regNum: String): Boolean = {
+    validateRegime(regime, regNum) && validateRegNum(regNum)
+  }
+
+  private def validateRegime(regime: Regime, regNum: String): Boolean = {
+    if (!regime.equals(Regime.MGD)) {
+      val ref = regNum.takeRight(REF_NO_LENGTH).toLong
+      val regimeFromRegNo = {
+        if (ref >= 3000000 && ref <= 3199999) {
+          "GBD"
+        } else if (ref >= 3200000 && ref <= 3399999) {
+          "PBD"
+        } else if (ref >= 3400000 && ref <= 3599999) {
+          "RGD"
+        } else {
+          ""
+        }
+      }
+
+      if (!regimeFromRegNo.trim.equals(regime.code.toUpperCase.trim)) {
+        logger.info(s"validateRegime Regime does not match RegNum '$regime':'$regimeFromRegNo' '$regNum' '$ref'")
+        false
+      } else {
+        logger.info(s"validateRegime Regime matches RegNum '$regime':'$regimeFromRegNo' '$regNum' '$ref'")
+        true
+      }
+    } else {
+      true
+    }
+  }
+
+  private def validateRegNum(regNum: String): Boolean = {
+    if (!regNum.isBlank && regNum.length == 14) {
+      if (regNum.toUpperCase.matches(regEx)) {
+        val char3 = (regNum.toUpperCase.substring(2, 3).toCharArray.head.toInt - 32) * WEIGHT_9
+        val sum = List.range(1, 11).map(x => weights(x) * regNum.substring(x + 2, x + 3).toInt).sum + char3
+        val checkChar = checkChars(sum % 23)
+        if (regNum.toUpperCase().substring(1, 2).equals(checkChar)) {
+          true
+        } else {
+          logger.info(s"validateRegNum '$regNum' has invalid check character actual=${regNum.toUpperCase().substring(1, 2)} calculated=$checkChar")
+          false
+        }
+      } else {
+        logger.info(s"validateRegNum '$regNum' does not match regEx")
+        false
+      }
+    } else {
+      logger.info(s"validateRegNum '$regNum' is blank or not 14 chars")
+      false
+    }
   }
 }
