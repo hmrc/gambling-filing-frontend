23821d113260caf5e40047536dd06d78637c6a8c
RajanCP <176382469+RajanCP@users.noreply.github.com>
2026-08-07 11:18:36 +0100

    Fixed controller to add refactoring

--- app/controllers/NegativeDutyController.scala
+++ app/controllers/NegativeDutyController.scala
@@ -36,6 +36,7 @@ class NegativeDutyController @Inject() (
   backNavigator: BackNavigator,
   authorise: AuthorisedAction,
   getData: DataRetrievalAction,
+  requireMgd: MgdRegimeAction,
   formProvider: NegativeDutyFormProvider,
   val controllerComponents: MessagesControllerComponents,
   view: NegativeDutyView
@@ -44,44 +45,44 @@ class NegativeDutyController @Inject() (
 
   private val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (authorise andThen getData).async { implicit request =>
-    whenMgd {
-      request.userAnswers.flatMap(_.get(SelectReturnPage)) match {
-        case None =>
-          logger.info(s"[onPageLoad] no selectedReturn found for regNum=${request.regNum}")
-          Future.successful(Redirect(controllers.routes.SelectReturnController.onPageLoad()))
-        case Some(selectedReturn) =>
-          val preparedForm = request.userAnswers.flatMap(_.get(NegativeDutyPage)).fold(form)(value => form.fill(value))
+  def onPageLoad(mode: Mode): Action[AnyContent] = (authorise andThen getData andThen requireMgd).async { implicit request =>
+    request.userAnswers.flatMap(_.get(SelectReturnPage)) match {
+      case None =>
+        logger.info(s"[onPageLoad] no selectedReturn found for regNum=${request.regNum}")
+        Future.successful(Redirect(controllers.routes.SelectReturnController.onPageLoad()))
+      case Some(selectedReturn) =>
+        val preparedForm = request.userAnswers.flatMap(_.get(NegativeDutyPage)).fold(form)(form.fill)
 
-          Future.successful(Ok(view(preparedForm, mode, backNavigator.backPage(NegativeDutyPage, mode, request), selectedReturn)))
-      }
+        Future.successful(
+          Ok(view(preparedForm, mode, backNavigator.backPage(NegativeDutyPage, mode, request), selectedReturn))
+        )
     }
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (authorise andThen getData).async { implicit request =>
-    whenMgd {
-      request.userAnswers.flatMap(_.get(SelectReturnPage)) match {
-        case None =>
-          logger.info(s"[onSubmit] no selectedReturn found for regNum=${request.regNum}")
-          Future.successful(Redirect(controllers.routes.SelectReturnController.onPageLoad()))
-        case Some(selectedReturn) =>
-          form
-            .bindFromRequest()
-            .fold(
-              formWithErrors =>
-                Future.successful(
-                  BadRequest(view(formWithErrors, mode, backNavigator.backPage(NegativeDutyPage, mode, request), selectedReturn))
-                ),
-              value => {
-                val userAnswers = request.userAnswers.getOrElse(UserAnswers(request.regNum))
+  def onSubmit(mode: Mode): Action[AnyContent] = (authorise andThen getData andThen requireMgd).async { implicit request =>
+    request.userAnswers.flatMap(_.get(SelectReturnPage)) match {
+      case None =>
+        logger.info(s"[onSubmit] no selectedReturn found for regNum=${request.regNum}")
+        Future.successful(Redirect(controllers.routes.SelectReturnController.onPageLoad()))
+      case Some(selectedReturn) =>
+        form
+          .bindFromRequest()
+          .fold(
+            formWithErrors =>
+              Future.successful(
+                BadRequest(
+                  view(formWithErrors, mode, backNavigator.backPage(NegativeDutyPage, mode, request), selectedReturn)
+                )
+              ),
+            value => {
+              val userAnswers = request.userAnswers.getOrElse(UserAnswers(request.regNum))
 
-                for {
-                  updatedAnswers <- Future.fromTry(userAnswers.set(NegativeDutyPage, value))
-                  _              <- sessionRepository.set(updatedAnswers)
-                } yield Redirect(navigator.nextPage(NegativeDutyPage, mode, updatedAnswers))
-              }
-            )
-      }
+              for {
+                updatedAnswers <- Future.fromTry(userAnswers.set(NegativeDutyPage, value))
+                _              <- sessionRepository.set(updatedAnswers)
+              } yield Redirect(navigator.nextPage(NegativeDutyPage, mode, updatedAnswers))
+            }
+          )
     }
   }
 }
