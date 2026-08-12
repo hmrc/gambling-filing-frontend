00db1f146196659685e1de0026d172bbf343cecd
RajanCP <176382469+RajanCP@users.noreply.github.com>
2026-08-12 16:01:51 +0100

    Replacing FAR-NEG-SCR tests in Nav spec

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -231,7 +231,6 @@ class NavigatorSpec extends SpecBase {
       }
 
       "must go from UnderDeclaredDutyPage to duty-brought-forward page when answer is No" in {
-        // TODO: /manage-gambling-tax/returns/duty-brought-forward (FAR-NEG-SCR) 20. FAR-NEG-SCR - File a Return - Negative duty brought forward (screener)
         val answers =
           emptyUserAnswers
             .set(UnderDeclaredDutyPage, false)
@@ -242,7 +241,7 @@ class NavigatorSpec extends SpecBase {
           UnderDeclaredDutyPage,
           NormalMode,
           answers
-        ) mustBe routes.PageNotFoundController.onPageLoad()
+        ) mustBe routes.NegativeDutyController.onPageLoad(NormalMode)
       }
 
       "must go from CalculatedMGDHigherRatePage to UnderDeclaredDutyController when answer is Yes" in {
@@ -364,13 +363,12 @@ class NavigatorSpec extends SpecBase {
         ) mustBe routes.JourneyRecoveryController.onPageLoad()
       }
 
-      // TODO Update this test once duty-brought-forward page(FAR-NEG-SCR) is added
       "must go from ContactHmrcPage to DutyBroughtForwardPage" in {
         navigator.nextPage(
           ContactHmrcPage,
           NormalMode,
           emptyUserAnswers
-        ) mustBe routes.IndexController.onPageLoad()
+        ) mustBe routes.NegativeDutyController.onPageLoad(NormalMode)
       }
 
       "must go from NegativeDutyBroughtForwardInputPage to CheckYourAnswersPage" in {
@@ -578,7 +576,6 @@ class NavigatorSpec extends SpecBase {
       }
 
       "must go from UnderDeclaredDutyPage to duty-brought-forward page when answer is No" in {
-        // TODO: /manage-gambling-tax/returns/duty-brought-forward   20. FAR-NEG-SCR - File a Return - Negative duty brought forward (screener)
         val answers =
           emptyUserAnswers
             .set(UnderDeclaredDutyPage, false)
@@ -589,7 +586,7 @@ class NavigatorSpec extends SpecBase {
           UnderDeclaredDutyPage,
           CheckMode,
           answers
-        ) mustBe routes.PageNotFoundController.onPageLoad()
+        ) mustBe routes.NegativeDutyController.onPageLoad(CheckMode)
       }
 
       "must go from CalculatedMGDHigherRatePage to UnderDeclaredDutyController when answer is Yes" in {
